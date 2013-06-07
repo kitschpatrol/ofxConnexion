bf1fdc7a13ce9cbccdc5b06ada81f1d8bb35d4d5
TAKAHASHI Tomohiro <edy555@gmail.com>
2013-06-07 15:56:59 +0900

    add ofxConnexionCam, which derived from ofEasyCam

---
+++ src/ofxConnexionCam.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ofEasyCam.h"
+#include "ofEvents.h"
+#include "ofxConnexion.h"
+
+class ofxConnexionCam : public ofEasyCam {
+public:
+    ofxConnexionCam();
+    ~ofxConnexionCam();
+    
+    void enableInput();
+	void disableInput();
+	bool getInputEnabled();
+
+private:
+	void connexionUpdate(ofEventArgs & args);
+	void updateRotation();
+	void updateTranslation();
+
+    bool bInputEnabled;
+    bool bDoRotate;
+    bool bDoTranslate;
+    bool bLookBack;
+};
